586bebde9593f272746772c52183123251f5f920
utsav <sav.acharya22@gmail.com>
2024-04-11 19:26:33 +1000

    content deletd

--- index.js
+++ index.js
@@ -18,14 +18,3 @@ export const initialItems = [
       packed: false,
     },
 
-    export function useItemsContext() {
-        const context = useContext(ItemsContext);
-      
-        if (!context) {
-          throw new Error(
-            "useItemsContext must be used within an ItemsContextProvider"
-          );
-        }
-      
-        return context;
-      }
