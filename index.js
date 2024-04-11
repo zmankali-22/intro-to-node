78df6cd050ce709becc00bd08f91b012f6d529d3
utsav <sav.acharya22@gmail.com>
2024-04-11 19:25:37 +1000

    items declared

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-"jeoo jello";
-
-const jello = "jeoo jello";
 
 
 
-edfefwaqef
+export const initialItems = [
+    {
+      id: 1,
+      name: "good mood",
+      packed: true,
+    },
+    {
+      id: 2,
+      name: "passport",
+      packed: false,
+    },
+    {
+      id: 3,
+      name: "phone charger",
+      packed: false,
+    },
