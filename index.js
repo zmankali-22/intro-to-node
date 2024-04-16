d8a0b1fe6a18c4978e71137a923031b99d98c06c
acharya <sav.acharya22@gmail.com>
2024-04-16 11:48:09 +1000

    feat: user input

--- index.js
+++ index.js
@@ -8,3 +8,28 @@ console.log("Terminal app is starting");
 pokemonPrinetrFile.pokemonPrinter();
 
 console.log("BYe BYe ! Terminal app is finished");
+
+var prompt = require('prompt-sync')({sigint: true});
+
+let userWantsToExit = false;
+
+
+do {
+    let n = (parseFloat(prompt("How number of Pokemon do you want to see?    ")))
+    console.log("You eneterd", n)
+    let userInputToExit = prompt("Would You like to try again? ")
+    if (userInputToExit === "y") {
+        userWantsToExit = false;
+    }
+    else {
+        userWantsToExit = true;
+    }
+
+}while (userWantsToExit === false);
+
+// console.log("User enetered:", n)
+
+
+// console.log(typeof(n))
+// let nasn = parseFloat(n)
+// console.log(typeof(nasn))
