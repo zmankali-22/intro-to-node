a3363bdc2e3df2dfb39c99d5f6488793edce7f0f
acharya <sav.acharya22@gmail.com>
2024-04-16 12:08:43 +1000

    add ne line

--- index.js
+++ index.js
@@ -9,44 +9,37 @@
 
 // console.log("BYe BYe ! Terminal app is finished");
 
-var prompt = require('prompt-sync')({sigint: true});
+var prompt = require("prompt-sync")({ sigint: true });
 
 function app() {
-    let userWantsToExit = false;
+  let userWantsToExit = false;
 
+  do {
+    let n = parseFloat(prompt("How number of Pokemon do you want to see?    "));
+    console.log(typeof n);
+    console.log("Input is not a number:", isNaN(n));
 
-    do {
-        let n = (parseFloat(prompt("How number of Pokemon do you want to see?    ")))
-        console.log(typeof(n))
-        console.log("Input is not a number:", isNaN(n))
-    
-        if (Number.isNaN(n) ) {
-          throw new Error("User did not eneter a number")
-        }
-        console.log("You eneterd", n)
-    
-    
-        let userInputToExit = prompt("Would You like to try again? ")
-        if (userInputToExit === "y") {
-            userWantsToExit = false;
-        }
-        else {
-            userWantsToExit = true;
-        }
-    
-    }while (userWantsToExit === false);
+    if (Number.isNaN(n)) {
+      throw new Error("User did not eneter a number");
+    }
+    console.log("You eneterd", n);
+
+    let userInputToExit = prompt("Would You like to try again? ");
+    if (userInputToExit === "y") {
+      userWantsToExit = false;
+    } else {
+      userWantsToExit = true;
+    }
+  } while (userWantsToExit === false);
 }
 try {
-    app()
+  app();
 } catch (error) {
-    console.log("Gracefully sutting down")
-    console.log(error.message)
+  console.log("Gracefully sutting down");
+  console.log(error.message);
 }
 
-
-
 // console.log("User enetered:", n)
-
 
 // console.log(typeof(n))
 // let nasn = parseFloat(n)
