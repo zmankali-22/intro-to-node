e56bb01106e0882ae813852194db9b4874fc8b03
acharya <sav.acharya22@gmail.com>
2024-04-16 12:07:31 +1000

    feat: refactor for try catch for nodemon script

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
-require("dotenv").config();
-const pokemonPrinetrFile = require("./pokemonPrinter");
+// require("dotenv").config();
+// const pokemonPrinetrFile = require("./pokemonPrinter");
 
-console.log(process.env.ENVIRONMENT_MESSAGE);
+// console.log(process.env.ENVIRONMENT_MESSAGE);
 
-console.log("Terminal app is starting");
+// console.log("Terminal app is starting");
 
-pokemonPrinetrFile.pokemonPrinter();
+// pokemonPrinetrFile.pokemonPrinter();
 
-console.log("BYe BYe ! Terminal app is finished");
+// console.log("BYe BYe ! Terminal app is finished");
 
 var prompt = require('prompt-sync')({sigint: true});
 
-let userWantsToExit = false;
+function app() {
+    let userWantsToExit = false;
 
 
-do {
-    let n = (parseFloat(prompt("How number of Pokemon do you want to see?    ")))
-    console.log("You eneterd", n)
-    let userInputToExit = prompt("Would You like to try again? ")
-    if (userInputToExit === "y") {
-        userWantsToExit = false;
-    }
-    else {
-        userWantsToExit = true;
-    }
+    do {
+        let n = (parseFloat(prompt("How number of Pokemon do you want to see?    ")))
+        console.log(typeof(n))
+        console.log("Input is not a number:", isNaN(n))
+    
+        if (Number.isNaN(n) ) {
+          throw new Error("User did not eneter a number")
+        }
+        console.log("You eneterd", n)
+    
+    
+        let userInputToExit = prompt("Would You like to try again? ")
+        if (userInputToExit === "y") {
+            userWantsToExit = false;
+        }
+        else {
+            userWantsToExit = true;
+        }
+    
+    }while (userWantsToExit === false);
+}
+try {
+    app()
+} catch (error) {
+    console.log("Gracefully sutting down")
+    console.log(error.message)
+}
 
-}while (userWantsToExit === false);
+
 
 // console.log("User enetered:", n)
 
